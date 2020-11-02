3171b0d2e2f14b975ddeb73cf6666d51abcd11b0
shivaraj shankar <51598598+shivaraj65@users.noreply.github.com>
2020-11-02 14:22:14 +0530

    kjef

---
+++ build/precache-manifest.40ed72653c40ec5d08c7bf1e9e950d05.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ca2a369d8d0c0c7ea45c02158e952fc",
+    "url": "/quizzler/index.html"
+  },
+  {
+    "revision": "7952374c193b4dbc03f2",
+    "url": "/quizzler/static/css/main.ac1967e3.chunk.css"
+  },
+  {
+    "revision": "fc210854bd473422c1b5",
+    "url": "/quizzler/static/js/2.d1aef5dd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/quizzler/static/js/2.d1aef5dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7952374c193b4dbc03f2",
+    "url": "/quizzler/static/js/main.7465e3cf.chunk.js"
+  },
+  {
+    "revision": "e451629f73b72906b216",
+    "url": "/quizzler/static/js/runtime-main.3aa80201.js"
+  },
+  {
+    "revision": "f320ae1f54e3232726b5500a32761884",
+    "url": "/quizzler/static/media/bg-masthead.f320ae1f.jpg"
+  },
+  {
+    "revision": "c41dda3fbc19e7ed5065d8626dd0aec5",
+    "url": "/quizzler/static/media/quizzler logo.c41dda3f.png"
+  },
+  {
+    "revision": "8f1222e97240e3b9f48df0ff43c3c585",
+    "url": "/quizzler/static/media/wolf.8f1222e9.svg"
+  }
+]);
